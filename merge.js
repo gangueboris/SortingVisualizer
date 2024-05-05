267e7e7678218a9d7fe65b65e8f0f1df919c6615
gangueboris <gangueboris@gmail.com>
2024-05-05 18:42:04 +0200

    update

---
+++ merge.js
@@ -0,0 +1,51 @@
+function merge (arrayBars, barsHeight, l, r)
+{
+    if (l < r)
+    {
+        const mid = l + Math.floor((r-l)/2);
+        bar_stateUpdate(arrayBars[mid], barsHeight[mid], '#fff');
+        merge (arrayBars,barsHeight, l, mid);
+        merge (arrayBars,barsHeight, mid+1, r);
+        mergeBoth (arrayBars,barsHeight, l, r, mid);
+    }
+    
+}
+
+function mergeBoth (arrayBars,barsHeight, l, r, mid)
+{
+    let leftArray = barsHeight.slice(l, mid+1);
+    let rightArray = barsHeight.slice(mid+1, r+1);
+    let i = 0, j = 0, currentIndex = l;
+
+    while (i < leftArray.length && j < rightArray.length)
+    {
+        if (leftArray[i] < rightArray[j])
+        {
+            barsHeight[currentIndex] = leftArray[i];
+            bar_stateUpdate(arrayBars[currentIndex], barsHeight[currentIndex], '#FFA500');
+            i++; 
+        }
+        else
+        {
+            barsHeight[currentIndex] = rightArray[j];
+            bar_stateUpdate(arrayBars[currentIndex], barsHeight[currentIndex], '#FFA500');
+            j++;
+        }
+        currentIndex++;
+    }
+     
+    while (i < leftArray.length)
+    {
+        barsHeight[currentIndex] = leftArray[i];
+        bar_stateUpdate(arrayBars[currentIndex], barsHeight[currentIndex], '#FFA500');
+        i++;
+        currentIndex++;
+    }
+    while (j < rightArray.length)
+    {
+        barsHeight[currentIndex] = rightArray[j];
+        bar_stateUpdate(arrayBars[currentIndex], barsHeight[currentIndex], '#FFA500');
+        j++;
+        currentIndex++;
+    }
+}
---
+++ quick.js
@@ -0,0 +1,44 @@
+function partition (arrayBars, barsHeight, low, high)
+{
+    const pivot = barsHeight[high];
+    let i = low -1;
+    bar_stateUpdate(arrayBars[high], barsHeight[high], '#f00'); // display the pivot
+
+    for (var j = low; j < high; j++)
+    { 
+        bar_stateUpdate(arrayBars[j], barsHeight[j], '#fff');
+        bar_stateUpdate(arrayBars[j], barsHeight[j], "linear-gradient(#3c77e6, #00aaff, #0ef)");
+
+       if (barsHeight[j] < pivot)
+       {
+            //i > 0 ? bar_stateUpdate(arrayBars[i], barsHeight[i], "linear-gradient(#3c77e6, #00aaff, #0ef)") : bar_stateUpdate(arrayBars[0], barsHeight[0], "linear-gradient(#3c77e6, #00aaff, #0ef)"); // update the position of the i
+            i++;
+            // display the swap
+            bar_stateUpdate(arrayBars[i], barsHeight[i], '#fff');
+            [barsHeight[i], barsHeight[j]] = [barsHeight[j], barsHeight[i]];
+            bar_stateUpdate(arrayBars[i], barsHeight[i], '#FFA500'); // maybe temporary // bar small than the pivot
+       }
+    }
+    bar_stateUpdate(arrayBars[j], barsHeight[j], "linear-gradient(#3c77e6, #00aaff, #0ef)");
+
+    bar_stateUpdate(arrayBars[i+1], barsHeight[i+1], '#fff');
+    [barsHeight[i+1], barsHeight[high]] = [barsHeight[high], barsHeight[i+1]];
+    bar_stateUpdate(arrayBars[i+1], barsHeight[i+1], '#FFA500');
+    bar_stateUpdate(arrayBars[high], barsHeight[high], "linear-gradient(#3c77e6, #00aaff, #0ef)");
+
+    //bar_stateUpdate(arrayBars[i+1], barsHeight[i+1], "linear-gradient(#3c77e6, #00aaff, #0ef)");
+
+    return i + 1;  // return pivot index;  
+}
+
+// My color code are: sorted items: '#FFA500; lessThanPivot : "#fff"; greaterThanPivot: '#000'; pivot: "#f00";
+
+function quick (arrayBars, barsHeight, low, high)
+{
+    if (low < high)
+    {
+        let pivotIndex = partition(arrayBars, barsHeight, low, high);
+        quick (arrayBars, barsHeight,low, pivotIndex - 1);
+        quick (arrayBars, barsHeight,pivotIndex + 1, high);
+    }
+}
